c21cde27f332709d63284b7eaf56bf0f48d99fc5
aa <838647225@qq.com>
2017-02-15 10:17:40 +0800

    1

--- src/Action/rpc.js
+++ src/Action/rpc.js
@@ -17,10 +17,10 @@ import {imei,version,client} from './auth'
 
 //export const ROOT_URL = 'http://jdy.tunnel.qydev.com/api/';
 //export const wsPath = "ws://"+'jdy.tunnel.qydev.com'+"/api/socketServer";
-// export const wsPath = "ws://"+'jdapi.tunnel.qydev.com'+"/api/socketServer";
-// export const ROOT_URL = 'http://jdapi.tunnel.qydev.com/api/'
-export const wsPath = "ws://"+'juduotest.tunnel.qydev.com'+"/api/socketServer";
-export const ROOT_URL = 'http://juduotest.tunnel.qydev.com/api/'
+export const wsPath = "ws://"+'jdapi.tunnel.qydev.com'+"/api/socketServer";
+export const ROOT_URL = 'http://jdapi.tunnel.qydev.com/api/'
+// export const wsPath = "ws://"+'juduotest.tunnel.qydev.com'+"/api/socketServer";
+// export const ROOT_URL = 'http://juduotest.tunnel.qydev.com/api/'
 //获取屏幕宽度
 export const SCREEN_WIDTH = window.screen.width
 
