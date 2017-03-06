4ba71fd3d07e778390a6bdbf8c58fcbc33def5dd
心无所恃 <chenrui_649@163.com>
2017-03-06 14:21:14 +0800

    删除目录src

--- src/Action/Const.js
+++
@@ -1,8 +0,0 @@
-export const config ={
-    /** 订单状态 */
-    //取消订单
-    order_status_cancel:5,
-    //待评价
-    order_status_receipt :3,
-
-}
--- src/Action/autoLogin.js
+++
@@ -1,9 +0,0 @@
-import {saveToken,getToken,clearToken,loadToken,ROOT_URL} from './rpc';
-import {imei} from './auth'
-
-//自动授权登录
-export function WechatAuth() {
-    const newPath = ROOT_URL + "wechat/wxCode";
-    const nowPath = window.location.href.split('?')[0];
-    location.href = newPath + "?callBackUrl=" + nowPath + "&imei=" + imei;
-}
